85d9fadab5970ac3ebcee02fe0637d0dbaf763e4
Lindah001 <lindachiry2018@gmail.com>
2023-02-18 11:29:32 -0800

    hey

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: Always 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
